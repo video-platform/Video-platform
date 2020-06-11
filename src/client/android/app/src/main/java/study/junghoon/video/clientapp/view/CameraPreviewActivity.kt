d85e7271344c4d05e78817cd39f5d510e12a037f
parkjunghoon <go9018@naver.com>
2020-06-11 23:23:24 +0900

    fix : make view package in activity
    - TODO : jetpack CameraX add

--- src/client/android/app/src/main/java/study/junghoon/video/clientapp/view/CameraPreviewActivity.kt
+++ src/client/android/app/src/main/java/study/junghoon/video/clientapp/view/CameraPreviewActivity.kt
@@ -1,10 +1,17 @@
 package study.junghoon.video.clientapp.view
 
+import android.graphics.Camera
+import android.hardware.Camera.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import study.junghoon.video.clientapp.R
+import java.lang.Exception
 
 class CameraPreviewActivity : AppCompatActivity() {
+    companion object {
+        const val CAMERA_ID = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera_preview)
