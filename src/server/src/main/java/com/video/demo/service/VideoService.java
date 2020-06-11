b1b448845b4e53ed8825bb0196c6a476bef3752e
SungWoo824 <scody1219@gmail.com>
2020-06-11 23:23:15 +0900

    video incoding 진행중

--- src/server/src/main/java/com/video/demo/service/VideoService.java
+++ src/server/src/main/java/com/video/demo/service/VideoService.java
@@ -1,4 +1,10 @@
 package com.video.demo.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface VideoService {
+    String videoUpload(MultipartFile multipartFile) throws IOException;
+    void videoIncoding(String videoId);
 }
