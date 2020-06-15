c632e549838e870d46a505e2b772fd157e55371b
parkjunghoon <go9018@naver.com>
2020-06-15 18:45:25 +0900

    add : login state - loginFragment add

--- src/client/android/app/src/main/java/study/junghoon/video/clientapp/view/MainActivity.kt
+++ src/client/android/app/src/main/java/study/junghoon/video/clientapp/view/MainActivity.kt
@@ -3,6 +3,7 @@ package study.junghoon.video.clientapp.view
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 
     private lateinit var retrofitApi: RetrofitApi
     private lateinit var mPermissionChecker: PermissionChecker
+    private var isLogin: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,18 +56,25 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         }
 
         R.id.action_login -> {
-            retrofitApi.loginRequest().enqueue(object : Callback<ServerResponse> {
+            if (isLogin) {
+                // 로그인상태 -> 계정 정보 view
+                openLoginView()
+            } else {
+                // 비로그인상태
+                openLoginView()
+            }
+            /*retrofitApi.loginRequest().enqueue(object : Callback<ServerResponse> {
                 override fun onFailure(call: Call<ServerResponse>, t: Throwable) {
-                    TODO("Not yet implemented")
+
                 }
 
                 override fun onResponse(
                     call: Call<ServerResponse>,
                     response: Response<ServerResponse>
                 ) {
-                    TODO("Not yet implemented")
+
                 }
-            })
+            })*/
             true
         }
 
@@ -85,11 +94,22 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         startActivity(intent)
     }
 
+    private fun openLoginView() {
+        supportFragmentManager.beginTransaction().add(R.id.main_layout, LoginFragment()).addToBackStack(null).commit()
+    }
+
     private fun init() {
         setSupportActionBar(my_toolbar)
         my_toolbar.title = getString(R.string.toolbarTitle)
         my_toolbar.setTitleTextColor(resources.getColor(R.color.color_white))
         mPermissionChecker = PermissionChecker(this, main_layout)
         retrofitApi = RetrofitBuilder.getInstance()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        }
+
     }
 }
