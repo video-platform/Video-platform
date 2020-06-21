6ca2a98a8a831690d10dd91c9f224d70c6acd740
SungWoo824 <scody1219@gmail.com>
2020-06-21 16:27:53 +0900

    add  : Redis Config, Repository

--- src/server/src/main/java/com/video/demo/domain/Video.java
+++ src/server/src/main/java/com/video/demo/domain/Video.java
@@ -3,25 +3,25 @@ package com.video.demo.domain;
 import javax.persistence.*;
 import java.sql.Timestamp;
 
-//@Entity
+@Entity
 public class Video {
-//    @Id
-//    private String videoId;
-//
-//    private int channelNo;
-//
-//    @OneToOne(cascade = CascadeType.PERSIST)
-//    @JoinColumn(name = "VIDEO_CATEGORY_NO")
-//    private final VideoCategory videoCategory;
-//
-//    private String videoName;
-//    private String videoContent;
-//    private Timestamp videoUploadDate;
-//    private String videoTag;
-//    private String videoAgelimit;
-//
-//
-//    public Video() {
-//        videoCategory = null;
-//    }
+    @Id
+    private String videoId;
+
+    private int channelNo;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "VIDEO_CATEGORY_NO")
+    private final VideoCategory videoCategory;
+
+    private String videoName;
+    private String videoContent;
+    private Timestamp videoUploadDate;
+    private String videoTag;
+    private String videoAgelimit;
+
+
+    public Video() {
+        videoCategory = null;
+    }
 }
