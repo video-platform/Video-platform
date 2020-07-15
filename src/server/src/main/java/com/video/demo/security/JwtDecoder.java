086df273d12db5e214a8c3b794129a203f7d0c1a
Jongmin Lee <jongminlee0218@gmail.com>
2020-07-15 22:32:33 +0900

    Refresh Token관련 로직 삭제

--- src/server/src/main/java/com/video/demo/security/JwtDecoder.java
+++ src/server/src/main/java/com/video/demo/security/JwtDecoder.java
@@ -6,9 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.video.demo.domain.Token;
 import com.video.demo.exception.InvalidJwtException;
-import com.video.demo.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +20,10 @@ public class JwtDecoder {
     private static final Logger log = LoggerFactory.getLogger(JwtDecoder.class);
 
     @Autowired
-    private TokenService tokenService;
-
-    @Autowired
     private JwtFactory jwtFactory;
 
-    public MemberContext decodeJwt(String token, String name){
-        DecodedJWT decodedJWT = isValidToken(token, name)
+    public MemberContext decodeJwt(String token){
+        DecodedJWT decodedJWT = isValidToken(token)
                 .orElseThrow(() -> new InvalidJwtException("유효한 토큰이 아닙니다."));
 
         String username = decodedJWT.getClaim("USER_EMAIL").asString();
@@ -37,39 +32,17 @@ public class JwtDecoder {
         return new MemberContext(username, "tempPassword", role);
     }
 
-    private Optional<DecodedJWT> isValidToken(String token, String email){
+    private Optional<DecodedJWT> isValidToken(String token){
         DecodedJWT jwt = null;
         try{
             Algorithm algorithm = Algorithm.HMAC256("secret");
-            // TODO : 시간 검증이 안된다.
-            // TODO: token 검증 logic 다시한번만 더 확인
+
             JWTVerifier verifier = JWT.require(algorithm)
                     .withIssuer("platform")
-                    .acceptExpiresAt(3600L * 24) // 1일
+                    .acceptExpiresAt(300L)
                     .build();
 
-            JWTVerifier refreshVerifier = JWT.require(algorithm)
-                    .withIssuer("refresh")
-                    .build();
-
-            Token compare = tokenService.getAccessToken(token);
-            if(!compare.getAccessToken().equals(token))
-                throw new InvalidJwtException("유효한 토큰이 아닙니다.");
-
-            try{
-                jwt = verifier.verify(token);
-            } catch (TokenExpiredException e){
-
-            } catch (JWTVerificationException ex){
-                // refresh_token
-                jwt = refreshVerifier.verify(token);
-                if(!compare.getRefreshToken().equals(token))
-                    throw new InvalidJwtException("유효한 토큰이 아닙니다.");
-
-                // 새로 발급해줘야 한다.
-                String accessToken = jwtFactory.generateToken(compare);
-
-            }
+            jwt = verifier.verify(token);
 
         }catch (Exception e){
             log.error(e.getMessage());
